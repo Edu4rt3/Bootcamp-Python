3d0f6842177afddb441d50370d86887ac77507e6
EduardoABD <eduardo.abduarte@gmail.com>
2025-04-09 19:58:30 -0300

    Corrigindo Day1

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator")
-city = input("Name of the city u born?")
-pet = input("Name of your pet?")
+city = input("Name of the city u born?\n")
+pet = input("Name of your pet?\n")
 print("Your band name is " +city +" " +pet)
