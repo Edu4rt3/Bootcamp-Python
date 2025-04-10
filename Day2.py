f2faa7130e7da4c338887e061deb7965fb919ea6
EduardoABD <eduardo.abduarte@gmail.com>
2025-04-10 17:32:28 -0300

    Day 2 correct

--- Day2.py
+++ Day2.py
@@ -3,5 +3,5 @@ value = float(input("What is the total bill? $"))
 tip = int(input("How much tip would u like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
 bill_w_tip = (tip / 100) * value + value
-total = bill_w_tip / people
+total = round(bill_w_tip / people, 2)
 print("Your total is $" + str(total))
