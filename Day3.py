6d69603efc17f9640b496d3894943fe980286540
EduardoABD <eduardo.abduarte@gmail.com>
2025-04-10 20:18:41 -0300

    Day 3

---
+++ Day3.py
@@ -0,0 +1,21 @@
+print("Welcome to Pizza deliveries")
+bill = 0
+size = input("What is the size of ur pizza? S, M or L: ")
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("U made the wrong choice")
+pepperoni = input("Do u want pepperoni in ur pizza? Y or N: ")
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+cheese = input("Do u want extra cheese? Y or N: ")
+if cheese == "Y":
+    bill += 1
+print(f"Your total bill is ${bill} dollars")
