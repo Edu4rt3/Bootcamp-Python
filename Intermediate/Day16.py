eefdb1de53f264b292495555e9b284f01a880c2e
EduardoABD <eduardo.abduarte@gmail.com>
2025-05-05 18:45:10 -0300

    Day 16

---
+++ Intermediate/Day16.py
@@ -0,0 +1,14 @@
+#OOP
+
+from turtle import *
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkGreen")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
