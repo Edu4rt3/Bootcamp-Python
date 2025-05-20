fe713c6312a2210fc6133783eeff516ad49080ca
EduardoABD <eduardo.abduarte@gmail.com>
2025-05-20 19:53:48 -0300

    Day 25

---
+++ Day_25/main.py
@@ -0,0 +1,51 @@
+# U.S. States Game
+
+import turtle
+import pandas
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+screen = turtle.Screen()
+screen.title("U.S. States Game ")
+image =  "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+t = turtle.Turtle()
+
+guessed_states = []
+score = 0
+game_is_on = True
+
+
+while game_is_on:
+    answer_states = screen.textinput(title="Gues the State", prompt="What's another state's name?r").title()
+
+    if answer_states == "Exit":
+        missing_states = []
+        for state in all_states:
+             if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        game_is_on = False
+        screen.clear()
+        t.hideturtle()
+        t.penup()
+        t.goto(0, 0)
+        t.write(f"The Game is over, your score is {score}", align="center", font=("Courier", 24, "normal"))
+
+    if answer_states in all_states:
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_states]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_states)
+        guessed_states.append(answer_states)
+        score += 1
+    
+screen.exitonclick()
+
+
+
+
