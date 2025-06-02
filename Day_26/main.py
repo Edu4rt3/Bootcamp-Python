b405f2e6d28700bf0c4134b8de0debded2784662
EduardoABD <eduardo.abduarte@gmail.com>
2025-06-02 17:18:44 -0300

    Day 36

--- Day_26/main.py
+++ Day_26/main.py
@@ -7,7 +7,14 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-name = input("What's your name?: ").upper()
-
-result = [phonetic_dict[letter] for letter in name]
-print(result)
+def generate_phonetic():
+    name = input("What's your name?: ").upper()
+    try:
+        result = [phonetic_dict[letter] for letter in name]
+        
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
+generate_phonetic()
